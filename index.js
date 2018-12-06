3d81675120ea32b941feee64b189465a3dd5378d
justcallmebeef <bethanymitch10@gmail.com>
2018-12-06 11:34:38 -0700

    got post to work

--- index.js
+++ index.js
@@ -12,4 +12,13 @@ app.get('/', (req, res) => {
     queries.listAll().then(response => res.send(response))
 })
 
+app.get('/:id', (req, res) => {
+    let id = req.params.id
+    queries.getById(id).then(response => res.send(response))
+})
+
+app.post('/', (req, res) => {
+    queries.createMovie(req.body).then(response => res.send(response[0]))
+})
+
 app.listen(port)
--- queries.js
+++ queries.js
@@ -3,5 +3,17 @@ const database = require('./db-connection')
 module.exports = {
     listAll() {
         return database('movies')
+    }, 
+    getById(id) {
+        return database('movies').where({id: id}).first()
+    }, 
+    createMovie(movie) {
+        return database('movies').insert(movie).returning('*')
+    }, 
+    deleteMovie(id) {
+        return database('movies').where('id', id).delete()
+    }, 
+    updateMovie(id, movie) {
+        return database('movies').where('id', id).update(student).returning('*')
     }
 }
