d4241abc5e80df642f16d92a68d011b5bd317742
justcallmebeef <bethanymitch10@gmail.com>
2018-12-06 10:39:47 -0700

    created and udpated seeds

---
+++ seeds/01_movies.js
@@ -0,0 +1,37 @@
+exports.seed = function(knex, Promise) {
+  return knex('movie_db').del()
+    .then(function () {
+      return knex('movie_db').insert([
+        {
+          title: "Schizopolis", 
+          director: "Steven Soderbergh",
+          year: "1976", 
+          rating: 4
+        }, 
+        {
+          title: "Nashville", 
+          director: "Robert Altman",
+          year: "1976", 
+          rating: 5
+        }, 
+        {
+          title: "Idiocracy", 
+          director: "Mike Judge",
+          year: "2006", 
+          rating: 5
+        }, 
+        {
+          title: "Love Actually", 
+          director: "Richard Curtis",
+          year: "2003", 
+          rating: 1
+        }, 
+        {
+          title: "Holy Mountain", 
+          director: "Alejandro Jodorowsky",
+          year: "1973", 
+          rating: 2
+        }
+      ]);
+    });
+};
