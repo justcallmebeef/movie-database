ea864daa32590329d4c38bc95c2c61b2f74b8635
justcallmebeef <bethanymitch10@gmail.com>
2018-12-06 11:23:55 -0700

    added bodyParser and cors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express') 
 const app = express() 
 const queries = require('./queries')
+const bodyParser = require('body-parser')
+const cors = require('cors') 
 let port = process.env.PORT || 3002
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     queries.listAll().then(response => res.send(response))
