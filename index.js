0b3488204295da90f3113220a03cb22e0ddfbed3
justcallmebeef <bethanymitch10@gmail.com>
2018-12-11 15:19:48 -0700

    updated error handling - still doesn't work

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3002
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     queries.listAll().then(allMovies => res.json(allMovies))
 })
 
@@ -20,12 +20,21 @@ app.post('/', (req, res) => {
     queries.createMovie(req.body).then(newMovie => res.json(newMovie[0]))
 })
 
-app.delete('/:id', (req, res) => {
+app.delete('/:id', (req, res, next) => {
     queries.deleteMovie(req.params.id).then(res.sendStatus(204))
 })
 
-app.put('/:id', (req, res) => {
+app.put('/:id', (req, res, next) => {
     queries.updateMovie(req.params.id, req.body).then(updatedMovie => res.json(updatedMovie[0]))
+})
+
+app.use((req, res, next) => {
+    res.status(404).json({ error: { message: 'data not found' }})
+})
+ 
+ app.use((err, req, res, next) => {
+    const status = err.status || 500
+    res.status(status).json({ error: err})
 })
 
 app.listen(port)
